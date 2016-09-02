5690b6efd108475758b0b849481ed8cd86aea9f9
DanRamage <ChiefDan@gmail.com>
2016-09-02 13:15:09 -0400

    Added check to see if we have more than 1 hours worth of data in file. Created an unprocessed records status to alert of potential telemetry ingestion issue.

--- telemetry_alerts.py
+++ telemetry_alerts.py
@@ -1,6 +1,6 @@
 import sys
-sys.path.append("/Users/danramage/Documents/workspace/CDMO/python/common")
-#sys.path.append("D:\scripts\common")
+#sys.path.append("/Users/danramage/Documents/workspace/CDMO/python/common")
+sys.path.append("D:\scripts\common")
 from os.path import join
 
 import logging.config
